f0c46f37c21ee5cfb37fe5fcbd6edcd12da8d33a
D <Dickerson.Morgan@outlook.com>
2023-01-30 21:29:38 -0500

    finished with mutation. also added resolvers export

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,11 +21,49 @@ const resolvers = {
             try {
                 const user = await User.create(args);
                 const token = signToken(user);
-                return { token, user};
+                return { token, user };
             } catch (err) {
                 console.log(err);
             }
         },
-    login: async
-    }
-}
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Try again');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Try again');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: args.input } },
+                    { new: true, runValidators: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('Please login first');
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('Please login first');
+        },
+    },
+};
+module.exports = resolvers;
